ef452f463da92e6fea2cbf4ced1e86fdcda70b8c
Denis Milanović <denis@milanovic.id.au>
2016-09-13 01:07:20 +1000

    Using RPS1 instead of RPROMPT to be consistent with PS1 usage.

--- zshrc.d/prompts.zsh
+++ zshrc.d/prompts.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 # Setting the Prompt
 PS1="%B%F{green}%n%f@%F{cyan}%m%f%b%(!.#.:) "
-RPROMPT="%B%h / %F{cyan}%~%f / %F{green}%D{%y-%m-%d} %D{%H:%M:%S}%b%f"
+RPS1="%B%h / %F{cyan}%~%f / %F{green}%D{%y-%m-%d} %D{%H:%M:%S}%b%f"

f11deeb3b39ab02cd7105f8d2d1e09328f507c3e
Denis Milanović <denis@milanovic.id.au>
2016-09-13 01:06:22 +1000

    Adding an orphaned package pacman alias.

--- zshrc.d/alias.zsh
+++ zshrc.d/alias.zsh
@@ -2,3 +2,6 @@
 # List Directory Contents
 alias ls='ls --color=auto --group-directories-first --dereference-command-line-symlink-to-dir -v'
 alias ll='ls -l --human-readable'
+
+# Pacman Wrappers
+alias orphans='pacman -Qtdg'

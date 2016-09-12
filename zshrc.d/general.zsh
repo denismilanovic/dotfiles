936d3b6e21d2a5a06b7c5501758204fea35b9ae5
Denis Milanović <denis@milanovic.id.au>
2016-09-13 01:05:53 +1000

    Adding shorter KEYTIMEOUT. Adding comments to general options.

--- zshrc.d/general.zsh
+++ zshrc.d/general.zsh
@@ -1,3 +1,15 @@
 #!/bin/zsh
+
+# Setting VI mode
 bindkey -v
-setopt autocd extendedglob
+
+# Reduced timeout to make VI work nicer
+export KEYTIMEOUT=1
+
+# Automatically cd into directories
+setopt autocd 
+
+# Fancier globbing
+setopt extendedglob
+
+

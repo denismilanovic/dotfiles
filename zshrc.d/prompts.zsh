9ffc878303e7636503ec91585de0c186a7884757
Denis Milanović <denis@milanovic.id.au>
2016-10-17 22:03:30 +1100

    Adding vi mode prompting to the right prompt.

--- zshrc.d/prompts.zsh
+++ zshrc.d/prompts.zsh
@@ -1,4 +1,12 @@
 #!/bin/zsh
 # Setting the Prompt
 PS1="%B%F{green}%n%f@%F{cyan}%m%f%b%(!.#.:) "
-RPS1="%B%h / %F{cyan}%~%f / %F{green}%D{%y-%m-%d} %D{%H:%M:%S}%b%f"
+
+function zle-line-init zle-keymap-select {
+	VI_MODE="${${KEYMAP/vicmd/%F{yellow}/(main|viins)/%F{white}}"
+	RPS1="%B${VI_MODE}%h%f / %F{cyan}%~%f / %F{green}%D{%y-%m-%d} %D{%H:%M:%S}%b%f"
+	RPS2=$RPS1
+	zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select

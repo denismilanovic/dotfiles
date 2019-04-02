2f208abd01ffe18b8fb1157ca2b0d569ab8c94fd
Denis Milanović <denis@milanovic.id.au>
2019-04-02 17:31:14 +1100

    Adding bash_profile.

--- install.sh
+++ install.sh
@@ -14,5 +14,6 @@ mklink ${config_dir}/ ${HOME}/.config
 mklink ${HOME}/local ${HOME}/.local
 mklink ${config_dir}/bashrc ${HOME}/.bashrc
 mklink ${config_dir}/profile ${HOME}/.profile
+mklink ${config_dir}/bash_profile ${HOME}/.bash_profile
 mklink ${config_dir}/tmux.conf ${HOME}/.tmux.conf
 mklink ${config_dir}/vim ${HOME}/.vim

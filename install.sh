c07e944fd1a1129f364b9766259958e21d9b119c
Denis Milanović <denis@milanovic.id.au>
2019-04-02 17:25:19 +1100

    Adding an install script for initial setup.

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+config_dir=$(readlink -f $(dirname $0))
+
+function mklink() 
+{
+	source=$1;
+	target=$2;
+	rm -rf $2;
+	ln -s "$1" "$2";
+}
+
+mklink ${config_dir}/ ${HOME}/.config
+mklink ${HOME}/local ${HOME}/.local
+mklink ${config_dir}/bashrc ${HOME}/.bashrc
+mklink ${config_dir}/profile ${HOME}/.profile
+mklink ${config_dir}/tmux.conf ${HOME}/.tmux.conf
+mklink ${config_dir}/vim ${HOME}/.vim

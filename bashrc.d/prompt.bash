d26c7fa81a6770c4e42528aa84e8d5c5833679c2
Denis Milanović <denis@milanovic.id.au>
2020-11-13 15:26:37 +1100

    Moving the \033_ and \033\ characters into PROMPT_COMMAND rather than PS1 to avoid occasional issues where bash loses track of where the line starts and the prompt ends. Filling these fields with a sane default title (user@host:path) matching the screen default in arch.

--- bashrc.d/prompt.bash
+++ bashrc.d/prompt.bash
@@ -1,3 +1,6 @@
 #!/bin/bash
-export PS1='\033_\033\\>>> '
+export PS1='>>> '
 export PS2='... '
+
+# Send a title between APT and ST characters to update the pane title and force a tmux refresh
+export PROMPT_COMMAND='printf "\033_%s@%s:%s\033\\" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"'

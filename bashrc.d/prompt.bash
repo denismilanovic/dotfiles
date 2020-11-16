cdc8d017238f5c322e46d6fe863451d1941dab04
Denis Milanović <denis@milanovic.id.au>
2020-11-16 20:29:18 +1100

    Adding a right-aligned git prompt via PROMPT_COMMAND, if __git_ps1 is defined.

--- bashrc.d/prompt.bash
+++ bashrc.d/prompt.bash
@@ -3,4 +3,4 @@ export PS1='↪ '
 export PS2='… '
 
 # Send a title between APT and ST characters to update the pane title and force a tmux refresh
-export PROMPT_COMMAND='printf "\033_%s@%s:%s\033\\" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"'
+export PROMPT_COMMAND='printf "\033_%s@%s:%s\033\\%*s%s\r" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}" "$(tput cols)" "$(declare -f __git_ps1 > /dev/null && __git_ps1 "[%s]")"'

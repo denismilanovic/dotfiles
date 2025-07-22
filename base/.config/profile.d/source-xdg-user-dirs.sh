4c5a360814a6d8fac9438e67732a7e3eb5c3de3b
Denis Milanović <denis@milanovic.au>
2025-07-22 15:28:15 +1000

    Make XDG dirs script POSIX compliant.

--- base/.config/profile.d/source-xdg-user-dirs.sh
+++ base/.config/profile.d/source-xdg-user-dirs.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-XDG_CONFIG_HOME=$HOME/.config
-source ${XDG_CONFIG_HOME}/user-home.dirs
-source ${XDG_CONFIG_HOME}/user-dirs.dirs
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+[ -f "${XDG_CONFIG_HOME}/user-home.dirs" ] && . "${XDG_CONFIG_HOME}/user-home.dirs"
+[ -f "${XDG_CONFIG_HOME}/user-dirs.dirs" ] && . "${XDG_CONFIG_HOME}/user-dirs.dirs"
+
